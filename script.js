c8d0c552ee2ad93e2b2da765a0ab889df3e5110a
Judith Uchechukwu Ezenwosu <88844700+Uceeyjudy@users.noreply.github.com>
2022-03-04 14:35:38 +0100

    added style and modified script file

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Declare the only 3 posible choices, rock paper and sissors
-const choices =["rock", "paper", "sissors"];
+let choices =["rock", "paper", "sissors"];
 const winners =[];
-//Declare a function that plays the game
+//Declare a function that plays the game and plays 5 rounds
 function game(){
-    for (let i=0; i<5; i++){
+    for (let i=1; i<=5; i++){
         playRound(i);
     }
-    logwins();
-    document.querySelector("button").textContent="Play Game Again"
+    logWins();
+    document.querySelector("button").textContent="Play Game Again";
 }
 // declare a function that plays a round
 function playRound(round){
@@ -20,19 +20,22 @@ function playRound(round){
 // declare a function that takes the player choice
 function playerChoice(){
     let input = prompt("Type either rock, paper or scissors");
-    while (input==null){
-        input = prompt("Please enter either rock, paper or scissor correctly")
+    let check = validateInput(input);
+    if(check==false){
+        input = prompt("Please enter a correct spelling of rock, paper or scissors");
+    }
+    else if (check==null){
+        input= prompt("You have to enter a value!")
+
+    }
+    else {checkWinner();
     }
     return input;
 }
-input = input.toLowerCase();
-let check = validateInpute(input);
-if (check==true){
-    console.log(input);
-}
+
 // declare a function that playes computer choice randomly
 function computerChoice(){
-    return choices[Math.floor(Math.random * choices.length)];
+    return choices[Math.floor((Math.random) * choices.length)];
 }
 // function that validates the input
 function validateInput(choice){
@@ -59,5 +62,20 @@ function checkWinner(choicePlay, choiceComp){
 }
 // declare another function to log or display wins
 function logWins(){
-    
+    let playerWins=winners.filter((item)=>item=="Player wins").length;
+    let computerWins=winners.filter((item)=>item == "Computer wins").length;
+    let ties = winners.filter((item) =>item == "It's a tie").length;
+    console.log("Results");
+    console.log("Player wins", playerWins);
+    console.log("Computer wins", computerWins);
+    console.log("It's a tie", ties);
 }
+// declare another function that displays the round
+function logRound(playerChoice, computerChoice, winner, round){
+    console.log("Player chose", playerChoice);
+    console.log("Computer chose", computerChoice);
+    console.log(winner, "Won the round");
+    console.log("Round", round);
+    console.log("-----------------------")
+}
+game()
