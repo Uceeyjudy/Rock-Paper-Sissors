5e417fb6a89c022d60e7a6305edb658b41a25f09
Judith Uchechukwu Ezenwosu <88844700+Uceeyjudy@users.noreply.github.com>
2022-03-04 15:37:34 +0100

    added features

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Declare the only 3 posible choices, rock paper and sissors
-const choices =["rock", "paper", "sissors"];
+const choices =["rock", "paper", "scissors"];
 const winners =[];
 //Declare a function that plays the game and plays 5 rounds
 function game(){
-    for (let i=1; i<=5; i++){
+    for (let i=1; i<6; i++){
         playRound(i);
     }
     logWins();
@@ -20,6 +20,7 @@ function playRound(round){
 // declare a function that takes the player choice
 function playerChoice(){
     let input = prompt("Type either rock, paper or scissors");
+    input= input.toLowerCase();
     let check = validateInput(input);
     if(check==false){
         input = prompt("Please enter a correct spelling of rock, paper or scissors");
@@ -35,7 +36,7 @@ function playerChoice(){
 
 // declare a function that playes computer choice randomly
 function computerChoice(){
-    const choices =["rock", "paper", "sissors"];
+    const choices =["rock", "paper", "scissors"];
     return choices[Math.floor((Math.random()) * choices.length)];
 }
 // function that validates the input
@@ -50,26 +51,26 @@ function validateInput(choice){
 // declare another function to check winner
 function checkWinner(choicePlay, choiceComp){
     if (choicePlay===choiceComp){
-        return "It's a tie";
+        return "Tie";
     }
-    else if ((choicePlay==="rock" && choiceComp==="scissors") ||
+    else if ((choicePlay=== "rock" && choiceComp==="scissors") ||
     (choicePlay==="paper" && choiceComp ==="rock") ||
     (choicePlay ==="scissors" && choiceComp==="paper")){
-        return "Player wins";
+        return "Player";
     }
     else{
-        return "Computer wins";
+        return "Computer";
     }
 }
 // declare another function to log or display wins
 function logWins(){
-    let playerWins=winners.filter((item)=>item=="Player wins").length;
-    let computerWins=winners.filter((item)=>item == "Computer wins").length;
-    let ties = winners.filter((item) =>item == "It's a tie").length;
+    let playerWins=winners.filter((item)=>item=="Player").length;
+    let computerWins=winners.filter((item)=>item == "Computer").length;
+    let ties = winners.filter((item) =>item == "Tie").length;
     console.log("Results");
     console.log("Player wins", playerWins);
     console.log("Computer wins", computerWins);
-    console.log("It's a tie", ties);
+    console.log("It's a Tie", ties);
 }
 // declare another function that displays the round
 function logRound(playerChoice, computerChoice, winner, round){
