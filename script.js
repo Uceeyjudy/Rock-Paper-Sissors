b67fbfb01e76adade288ea94177a73aed2b12b05
Judith Uchechukwu Ezenwosu <88844700+Uceeyjudy@users.noreply.github.com>
2022-03-07 12:20:44 +0100

    added my first function

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const computerChoice=document.getElementById("computer-choice");
-const userChoice=document.getElementById("user-choice");
+const computerChoiceDisplay=document.getElementById("computer-choice");
+const userChoiceDisplay=document.getElementById("user-choice");
 const result=document.getElementById("result");
-const choices=document.querySelectorAll("player")
+const choices=document.querySelectorAll("button");
+let userChoice;
+choices.forEach(possibleChoice=>possibleChoice.addEventListener('click', (e)=>{
+    userChoice = e.target.id
+    userChoiceDisplay.innerText=userChoice
+    generateComputerChoice()
+}));
+function generateComputerChoice(){
+    const computerChoice=Math.floor(Math.random() * choices.length)
+}
