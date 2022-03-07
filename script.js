2b557fa2f5556c22d1c8f80ee0735d5f6bb2998c
Judith Uchechukwu Ezenwosu <88844700+Uceeyjudy@users.noreply.github.com>
2022-03-07 13:07:24 +0100

    This a simple and functional game of rock paper scissors

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 const computerChoiceDisplay=document.getElementById("computer-choice");
 const userChoiceDisplay=document.getElementById("user-choice");
-const result=document.getElementById("result");
+const resultDisplay=document.getElementById("result");
 const choices=document.querySelectorAll("button");
 let userChoice;
+let result;
+
 choices.forEach(possibleChoice=>possibleChoice.addEventListener('click', (e)=>{
     userChoice = e.target.id
-    userChoiceDisplay.innerText=userChoice
+    userChoiceDisplay.innerHTML=userChoice
     generateComputerChoice()
+    getResult()
 }));
 function generateComputerChoice(){
-    const computerChoice=Math.floor(Math.random() * choices.length)
+    const randomNum=Math.floor(Math.random() * (choices.length+1))
+    if(randomNum === 1){
+        computerChoice= "rock"
+    }
+    else if (randomNum=== 2){
+        computerChoice= 'paper'
+    }
+    else if (randomNum === 3){
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML=computerChoice
+}
+function getResult(){
+    if ((userChoice=='rock'&& computerChoice == 'scissors')
+    ||(userChoice =='paper'&& computerChoice=='rock')
+    ||(userChoice =='scissors'&& computerChoice=='paper')){
+        result = "You Won"
+    }
+    else if ((computerChoice=='rock'&& userChoice == 'scissors')
+    ||(computerChoice =='paper'&& userChoice=='rock')
+    ||(computerChoice =='scissors'&& userChoice=='paper')){
+        result = "You Lost"
+    }
+    else if (computerChoice===userChoice){
+        result = "It's a Tie"
+    }
+    resultDisplay.innerHTML=result
 }
